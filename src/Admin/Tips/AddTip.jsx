a1ad26f2249e673150b46d249713b9126479f51c
Kibet <137826199+Kkibe@users.noreply.github.com>
2025-05-07 17:00:12 +0300

    Update AddTip.jsx

--- src/Admin/Tips/AddTip.jsx
+++ src/Admin/Tips/AddTip.jsx
@@ -58,6 +58,7 @@ export default function AddTip() {
         status,
         time: timeOnly,
         won,
+        premium,
         results,
         type: gamesType
     }, setNotification, setLoading);
