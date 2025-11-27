f23c5c613deb541b8cd73c5e9e6e40105476e27d
coongames8 <coongames8@gmail.com>
2025-11-27 10:41:17 +0300

    Update Subscription.jsx

--- src/pages/Pay/Subscription.jsx
+++ src/pages/Pay/Subscription.jsx
@@ -48,7 +48,7 @@ export default function Subscription() {
     reference: (new Date()).getTime().toString(),
     email: user ? user.email : "coongames8@gmail.com",
     amount: (data && data.price * 100) || (subscription.price * 100),
-    publicKey: 'pk_live_f36eadef9a97cb84ef23ebec889bfc4e458e3a4a',//pk_live_bf58b947a76117c2c9e290979af963711bfd70d4
+    publicKey: 'pk_live_89946a1c460d9c43482268f4991f86de27408c64',//pk_live_f36eadef9a97cb84ef23ebec889bfc4e458e3a4a
     currency: "KES",
     metadata: {
       name: user ? user.email : "coongames8@gmail.com",
